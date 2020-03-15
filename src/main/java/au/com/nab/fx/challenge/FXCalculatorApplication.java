7a06d1d97e3f4157f635a63b215635acf763e26c
Revanth <trevanthkumar@gmail.com>
2020-03-15 12:12:54 +0530

    creating blank springboot project

---
+++ src/main/java/au/com/nab/fx/challenge/FXCalculatorApplication.java
@@ -0,0 +1,13 @@
+package au.com.nab.fx.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FXCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FXCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/au/com/nab/fx/challenge/FXCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.nab.fx.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FXCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
