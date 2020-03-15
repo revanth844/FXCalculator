979401a0a1ab19fe94ebca8dc11ff742dbe61198
Revanth <trevanthkumar@gmail.com>
2020-03-15 14:03:56 +0530

    defining constants

---
+++ src/main/java/au/com/nab/fx/challenge/util/Constants.java
@@ -0,0 +1,15 @@
+package au.com.nab.fx.challenge.util;
+
+import java.math.MathContext;
+
+public class Constants {
+
+	public static final String DIRECTION_BUY = "BUY";
+	public static final String DIRECTION_SELL = "SELL";
+
+	public static final String TRADE_EVENT_NEW = "NEW";
+	public static final String TRADE_EVENT_AMEND = "AMEND";
+	public static final String TRADE_EVENT_CANCEL = "CANCEL";
+
+	public static final MathContext PRECISION_FOUR = new MathContext(4);
+}
